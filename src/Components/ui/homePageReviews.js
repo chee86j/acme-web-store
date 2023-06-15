c0305bf24f0d99f8e48f7c1cfee770f86aa73c8e
cdriesner <driesnercalvin@gmail.com>
2023-06-15 13:59:28 -0700

    added some wild styles

--- src/Components/ui/homePageReviews.js
+++ src/Components/ui/homePageReviews.js
@@ -18,18 +18,21 @@ const HomePageReviews = () => {
 
   console.log(reviews)
 
-  return(
-    <>
-      {reviews.slice(0,6).map((review) => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="font-3d text-9xl text-secondary">
+        Reviews
+      </div>
+      {reviews.slice(0, 6).map((review) => {
         return (
-          <div>
+          <div className="glass m-4 w-[1500px] rounded-lg p-4 backdrop-blur">
             <h2 className="text-4xl">{review.user.username}</h2>
             <div className="text-2xl">{review.product.name}</div>
             <div>{review.description}</div>
           </div>
         )
       })}
-    </>
+    </div>
   )
 }
 export default HomePageReviews
